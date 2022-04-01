3cb90feebb9b6fe5bf551bf633687aa6dcdc4ff5
franc <onyishif@gmail.com>
2022-04-01 19:08:56 +0100

    get properties

--- src/main/java/com/laslibs/LasJava.java
+++ src/main/java/com/laslibs/LasJava.java
@@ -3,16 +3,12 @@ package com.laslibs;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class LasJava
 {
-    private String lasString;
+    private final String lasString;
 
     public LasJava(String fileSource, Boolean loadFile){
         if(loadFile){
@@ -25,16 +21,6 @@ public class LasJava
             return;
         }
         this.lasString = fileSource;
-    }
-
-    public String[] metaData(){
-        String metaPart = this.lasString.split("~V(?:\\w*\\s*)*\\n\\s*")[1]
-                        .split("~")[0];
-        metaPart = removeComment(metaPart);
-        List<String[]> refinedMeta = Arrays.stream(metaPart.split("\n"))
-                .map(m -> getSliceOfArray(m.split("\s{2,}|\s*:"),0,2 ))
-                .collect(Collectors.toList());
-        return refinedMeta.stream().map(r -> r[1]).toArray(String[]::new);
     }
 
     private <T> T[] getSliceOfArray(T[] arr, int startIndex, int endIndex) {
@@ -52,5 +38,68 @@ public class LasJava
                 .map(val -> val.replaceAll("^\\s+", ""))
                 .filter(f -> !(f.charAt(0) == '#'))
                 .collect(Collectors.joining("\n"));
+    }
+
+    private String[] metaData(){
+        String metaPart = this.lasString.split("~V(?:\\w*\\s*)*\\n\\s*")[1]
+                .split("~")[0];
+        metaPart = removeComment(metaPart);
+        List<String[]> refinedMeta = Arrays.stream(metaPart.split("\n"))
+                .map(m -> getSliceOfArray(m.split("\s{2,}|\s*:"),0,2 ))
+                .collect(Collectors.toList());
+        return refinedMeta.stream().map(r -> r[1]).toArray(String[]::new);
+    }
+
+    public Boolean getWrap(){
+        String[] meta = metaData();
+        return meta[1].equalsIgnoreCase("yes");
+    }
+
+    public Double getVersion(){
+        String[] meta = metaData();
+        return Double.parseDouble(meta[0]);
+    }
+
+    private Map<String, Map<String, String>> getProperty(String type) {
+        // Set regex for the different properties
+        HashMap<String, String> props = new HashMap<>() {{
+            put("curve", "~C(?:\\w*\\s*)*\\n\\s*");
+            put("param", "~P(?:\\w*\\s*)*\\n\\s*");
+            put("well", "~W(?:\\w*\\s*)*\\n\\s*");
+        }};
+        String[] ls = lasString.split(props.get(type));
+        String wp = "";
+        if (ls.length > 0) {
+            wp = ls[1].split("~")[0];
+            wp = removeComment(wp);
+        }
+        Map<String, Map<String, String>> wellProps = new HashMap<>();
+        if (!(wp.length() > 0)) {
+            throw new LasException("The property " + type + " does not exist");
+        }
+        Arrays.stream(wp.split("\n")).forEach((str) -> {
+            String obj = str.replaceAll("\\s *[.]\\s +", "   none   ");
+            String title = obj.split("[.]|\\s +")[0];
+            String unit = obj.trim().split("^\\w+\\s*[.]*s*")[1]
+                    .split("\\s +")[0];
+            String description = obj.split("[:]")[1].trim().isEmpty() ? "none" : obj.split("[:]")[1].trim();
+            String[] valPrep = obj.split("[:]")[0].split("\\s{2,}\\w*\\s{2,}");
+            String value = valPrep.length > 2 && (valPrep[valPrep.length - 1].isEmpty() || valPrep[valPrep.length - 1] == null) ? valPrep[valPrep.length - 2] : valPrep[valPrep.length - 1];
+            String finalValue = value.length() > 0 ? value.trim() : value;
+            wellProps.put(title, new HashMap<>() {{
+                put("unit", unit);
+                put("value", finalValue);
+                put("description", description);
+            }});
+        });
+        return wellProps;
+    }
+
+    public Map<String, Map<String, String>> getCurveParams(){
+        return getProperty("curve");
+    }
+
+    public Map<String, Map<String, String>> getWellParams(){
+        return getProperty("well");
     }
 }
--- src/test/java/com/laslibs/LasJavaTest.java
+++ src/test/java/com/laslibs/LasJavaTest.java
@@ -2,25 +2,23 @@ package com.laslibs;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * Unit test for simple App.
  */
 public class LasJavaTest
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
-
-    @Test
-    public void getMeta(){
-        String s = "~VERSION INFORMATION\n" +
+    // String
+    String lasString;
+    @Before
+    public void setup(){
+        lasString = "~VERSION INFORMATION\n" +
                 "VERS.                          2.0 :   CWLS LOG ASCII STANDARD -VERSION 2.0\n" +
                 "WRAP.                          NO  :   ONE LINE PER DEPTH STEP\n" +
                 "~WELL INFORMATION\n" +
@@ -31,8 +29,71 @@ public class LasJavaTest
                 "STEP    .M              -0.1250                  :STEP\n" +
                 "NULL    .               -999.25                  :NULL VALUE\n" +
                 "COMP    .       ANY OIL COMPANY INC.             :COMPANY\n" +
-                "WELL    .       ANY ET AL 12-34-12-34            :WELL";
-        LasJava las = new LasJava(s, false);
-        String[] st = las.metaData();
+                "WELL    .       ANY ET AL 12-34-12-34            :WELL\n" +
+                "FLD     .       WILDCAT                          :FIELD\n" +
+                "LOC     .       12-34-12-34W5M                   :LOCATION\n" +
+                "PROV    .       ALBERTA                          :PROVINCE\n" +
+                "SRVC    .       ANY LOGGING COMPANY INC.         :SERVICE COMPANY\n" +
+                "DATE    .       13-DEC-86                        :LOG DATE\n" +
+                "UWI     .       100123401234W500                 :UNIQUE WELL ID\n" +
+                "~CURVE INFORMATION\n" +
+                "#MNEM.UNIT              API CODES                   CURVE DESCRIPTION\n" +
+                "#------------------     ------------              -------------------------\n" +
+                " DEPT   .M                                       :  1  DEPTH\n" +
+                " DT     .US/M           60 520 32 00             :  2  SONIC TRANSIT TIME\n" +
+                " RHOB   .K/M3           45 350 01 00             :  3  BULK DENSITY\n" +
+                " NPHI   .V/V            42 890 00 00             :  4  NEUTRON POROSITY\n" +
+                " SFLU   .OHMM           07 220 04 00             :  5  SHALLOW RESISTIVITY\n" +
+                " SFLA   .OHMM           07 222 01 00             :  6  SHALLOW RESISTIVITY\n" +
+                " ILM    .OHMM           07 120 44 00             :  7  MEDIUM RESISTIVITY\n" +
+                " ILD    .OHMM           07 120 46 00             :  8  DEEP RESISTIVITY\n" +
+                "~PARAMETER INFORMATION\n" +
+                "#MNEM.UNIT              VALUE             DESCRIPTION\n" +
+                "#--------------     ----------------      -----------------------------------------------\n" +
+                " MUD    .               GEL CHEM        :   MUD TYPE\n" +
+                " BHT    .DEGC           35.5000         :   BOTTOM HOLE TEMPERATURE\n" +
+                " BS     .MM             200.0000        :   BIT SIZE\n" +
+                " FD     .K/M3           1000.0000       :   FLUID DENSITY\n" +
+                " MATR   .               SAND            :   NEUTRON MATRIX\n" +
+                " MDEN   .               2710.0000       :   LOGGING MATRIX DENSITY\n" +
+                " RMF    .OHMM           0.2160          :   MUD FILTRATE RESISTIVITY\n" +
+                " DFD    .K/M3           1525.0000       :   DRILL FLUID DENSITY\n" +
+                "~OTHER\n" +
+                "     Note: The logging tools became stuck at 625 metres causing the data\n" +
+                "     between 625 metres and 615 metres to be invalid.\n" +
+                "~A  DEPTH     D/T    RHOB3        NPHI   SFLU    SFLA      ILM      ILD\n" +
+                "1670.000   123.450 2550.000    0.450  123.450  123.450  110.200  105.600\n" +
+                "1669.875   123.450 2550.000    0.450  123.450  123.450  110.200  105.600\n" +
+                "1669.750   123.450 2550.000    0.450  123.450  123.450  110.200  105.600\n" +
+                "1669.745   123.450 2550.000    -999.25  123.450  123.450  110.200  105.600";
+    }
+
+    @Test
+    public void getMetaVersion(){
+        LasJava las = new LasJava(lasString, false);
+        Double version = las.getVersion();
+        Assert.assertNotNull(version);
+        Assert.assertEquals(version, Double.valueOf("2.0"));
+    }
+
+    @Test
+    public void getMetaWrap(){
+        LasJava las = new LasJava(lasString, false);
+        Boolean version = las.getWrap();
+        Assert.assertFalse(version);
+    }
+
+    @Test
+    public void getCurveParams(){
+        LasJava las = new LasJava(lasString, false);
+        Map<String, Map<String, String>> value = las.getCurveParams();
+        Assert.assertEquals(value.get("DT").get("unit"), "US/M");
+    }
+
+    @Test
+    public void getWellParams(){
+        LasJava las = new LasJava(lasString, false);
+        Map<String, Map<String, String>> value = las.getWellParams();
+        Assert.assertEquals(value.get("DATE").get("value"), "13-DEC-86");
     }
 }
